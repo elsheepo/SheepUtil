64eac11b7662c4d04cc914df3c7b5234f33bd241
beatzz <elsheepo[gmail.com>
2018-08-28 14:34:20 +0900

    Made some changes to a few of the methods in the Tools class, also added some JavaDoc comments.

--- src/Utilities/Tools.java
+++ src/Utilities/Tools.java
@@ -14,25 +14,33 @@ import java.time.format.FormatStyle;
 public class Tools {
 
     private static NumberFormat number = NumberFormat.getNumberInstance();
-
     private static DateTimeFormatter dateFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG);
     private static DateTimeFormatter timeFormatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
     private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG, FormatStyle.SHORT);
-
     private static LocalDate currentDate = LocalDate.now();
     private static LocalTime currentTime = LocalTime.now();
     private static LocalDateTime currentDateTime = LocalDateTime.now();
 
+    /**
+     *  The genRandomInt(int max) method, designed to generate a random integer, given an inclusive maximum integer value as an argument.
+     * @param max Designates the inclusive maximum integer value possible for the return.
+     * @return Returns a random integer greater than or equal to 0, and less than or equal to the argument supplied by the user.
+     */
     public static int genRandomInt(int max) {
-        double d = Math.random() * max;
-        int randomInt = (int) d;
-        randomInt++;
-        return randomInt;
+        return (int)Math.round(Math.random() * max);
     }
 
+    /**
+     *  The genRandomInt(int min, int max), designed to generate a random integer, given minimum and maximum integer values as arguments.
+     *  This method will return an integer value within the specified range.
+     * @param min designates the inclusive minimum integer value possible for the return.
+     * @param max designates the inclusive maximum integer value possible for the return.
+     * @return Returns a random integer between the inclusive minimum and inclusive maximum arguments supplied by the user.
+     */
     public static int genRandomInt(int min, int max) {
-        double d = Math.random() * max;
-        int i = (int) d;
+        double minDouble = Math.random() * min;
+        double maxDouble = Math.random() * max;
+        int i = (int) ((minDouble * maxDouble) / 2);
         i++;
         if (i < min)
             while (i < min)
@@ -40,10 +48,22 @@ public class Tools {
         return i;
     }
 
+    /**
+     *  The genRandomDouble(int max) method, designed to generate a random double, given an inclusive maximum double value as an argument.
+     * @param max Designates the inclusive maximum double value possible for the return.
+     * @return Returns a random double greater than or equal to 0, and less than or equal to the argument supplied by the user.
+     */
     public static double genRandomDouble(double max) {
         return new BigDecimal(Math.random() * max).setScale(2, RoundingMode.HALF_EVEN).doubleValue();
     }
 
+    /**
+     *  The genRandomDouble(int min, int max), designed to generate a random double, given minimum and maximum double values as arguments.
+     *  This method will return a double value within the specified range.
+     * @param min designates the inclusive minimum double value possible for the return.
+     * @param max designates the inclusive maximum double value possible for the return.
+     * @return Returns a random double between the inclusive minimum and inclusive maximum arguments supplied by the user.
+     */
     public static double genRandomDouble(double min, double max) {
         double d = new BigDecimal(Math.random() * max).setScale(2, RoundingMode.HALF_EVEN).doubleValue();
         if (d < min)
@@ -55,23 +75,33 @@ public class Tools {
         return d;
     }
 
+    /**
+     *  the calculateFutureValue(double monthlyInvestment, double yearlyInterestRate, int years) method.
+     *  Designed to calculate and return the future value of a monthly investment incurring compound interested.
+     * @param monthlyInvestment The monthly amount deposited into the investment.
+     * @param yearlyInterestRate The annual interest rate.
+     * @param years The span of years over which the investment is allowed to grow.
+     * @return Returns a double value of the calculated future value of the investment.
+     */
     public static double calculateFutureValue(double monthlyInvestment, double yearlyInterestRate, int years) {
-        // convert yearly values to monthly values
         double monthlyInterestRate = yearlyInterestRate / 12 / 100;
         int months = years * 12;
-
-        // calculate the future value
         double futureValue = 0;
         for (int i = 1; i <= months; i++) {
             futureValue += monthlyInvestment;
             double monthlyInterestAmount = futureValue * monthlyInterestRate;
             futureValue += monthlyInterestAmount;
         }
-
         return futureValue;
     }
 
-    public static String pad(String s, int length) {
+    /**
+     * Pads a string to the specified length supplied by the user.
+     * @param s the string to be padded.
+     * @param length the length to pad the string to.
+     * @return Returns a string padded to the specified length.
+     */
+    public static String padString(String s, int length) {
         if (s.length() < length) {
             StringBuilder sb = new StringBuilder(s);
             while (sb.length() < length) {
@@ -83,15 +113,15 @@ public class Tools {
         }
     }
 
-    public static String displayCurrentDate() {
+    public static String getCurrentDate() {
         return dateFormatter.format(currentDate);
     }
 
-    public static String displayCurrentTime() {
+    public static String getCurrentTime() {
         return timeFormatter.format(currentTime);
     }
 
-    public static String displayCurrentDateTime() {
+    public static String getCurrentDateTime() {
         return dateTimeFormatter.format(currentDateTime);
     }
 
@@ -99,8 +129,9 @@ public class Tools {
      *  The rightSpot() method! I put this one together helping a fellow coder with an exam question.
      *  it will return the int value stored in an array, in the event that all the previous values are less than, and
      *  all the subsequent values are greater than.
-     * @param x
-     * @return
+     * @param x integer array supplied by the user.
+     * @return Returns the array index at which the previous values are less than, and all the
+     * subsequent values are greater than.
      */
     public static int rightSpot(int[] x) {
         int result = -1;
@@ -124,6 +155,13 @@ public class Tools {
         return result;
     }
 
+    /**
+     * The writeToTextFile(Path filePath, String s) method. Designed to establish a connection to a text file, and print
+     * the supplied String argument to said file.
+     * @param filePath Either the Fully Qualified Path Name (FQPN), or relative path from the project directory to the
+     * text file.
+     * @param s The string to which the text fill will be appended.
+     */
     public static void writeToTextFile(Path filePath, String s) {
         File textFile = filePath.toFile();
         try (PrintWriter toTextFile = new PrintWriter(
